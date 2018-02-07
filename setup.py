068ad3d7c4b501f5e4d864e0d0087b3ffae46efc
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2018-02-07 15:59:46 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+import numpy
+import _version
+USE_CYTHON = 'auto'
+
+
+if USE_CYTHON:
+    try:
+        from Cython.Distutils import build_ext
+        if USE_CYTHON=='auto':
+            USE_CYTHON=True
+    except ImportError:
+        if USE_CYTHON=='auto':
+            USE_CYTHON=False
+        else:
+            raise
+
+cmdclass = {}
+ext_modules = []
+
+if USE_CYTHON:
+    ext_modules += [
+        Extension("wsmtk.whittaker", ["wsmtk/_whittaker.pyx"],extra_compile_args = ["-O3", "-ffast-math"])]
+    cmdclass.update({'build_ext': build_ext })
+else:
+    ext_modules += [
+        Extension("wsmtk.whittaker", ["smtk/_whittaker.c"],extra_compile_args = ["-O3", "-ffast-math"])]
+
+
+setup(
+    name='wsmkt',
+    description='Whittaker Smoothing Toolkit',
+    version=_version.__version__,
+    author='Valentin Pesendorfer',
+    author_email='valentin.pesendorfer@wfp.org',
+    long_description=open('README.rst').read(),
+    include_dirs=[numpy.get_include()],
+    packages=['wsmtk'],
+    cmdclass = cmdclass,
+    ext_modules=ext_modules,
+    classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+],
+    install_requires=['numpy'],
+)
