b41d9682da40cf26868feca852979387de60f5c2
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2018-02-16 12:09:02 +0100

    add download and set credentials, modifications

--- wsmtk/modis.py
+++ wsmtk/modis.py
@@ -1,17 +1,21 @@
-from __future__ import print_function,division
+from __future__ import print_function
 import requests
 from bs4 import BeautifulSoup
 import re
-import sys
-
+import sys, os
+import time
+from subprocess import Popen, check_output
 
 class MODISquery:
 
-    def __init__(self,url):
-
-        r = re.compile(".+(h\d+v\d+).+")
+    def __init__(self,url,username=None,password=None,targetdir=os.getcwd()):
 
         self.queryURL = url
+        self.username = username
+        self.password = password
+        self.targetdir = targetdir
+
+        r = re.compile(".+(h\d+v\d+).+")
 
         print('Checking for MODIS products ...')
         try:
@@ -35,3 +39,34 @@ class MODISquery:
             print('%s results found.' % self.results)
         else:
             print('0 results found. Please check query!')
+
+
+    def setCredentials(self,username,password):
+        self.username=username
+        self.password=password
+
+    def download(self):
+
+        try:
+            temp = check_output(['wget', '--version'])
+        except:
+            print("Download needs wget to be available in PATH! Please make sure it's installed and available in PATH!")
+            sys.exit(1)
+
+        if self.username is None or self.password is None:
+            print('No credentials found. Please run .setCredentials(username,password)!')
+            sys.exit(1)
+
+
+        args = ['wget','--user',self.username,'--password',self.password,'-P',self.targetdir]
+
+        print('[%s]: Downloading products to %s ...\n' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),self.targetdir))
+
+        for ix,u in enumerate(self.modisURLs):
+            print('%s of %s' %(ix+1,self.results))
+            p = Popen(args + [u])
+            p.wait()
+            if p.returncode is not 0:
+                print("Couldn't donwload %s - continuing." % u)
+
+        print('\n[%s]: Downloading finished.' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
