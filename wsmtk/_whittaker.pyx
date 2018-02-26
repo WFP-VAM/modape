7166ed8e35faf00cc34936d0d5d558ab28aa75e7
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2018-02-26 11:55:42 +0100

    add ws2d_internal

--- wsmtk/_whittaker.pyx
+++ wsmtk/_whittaker.pyx
@@ -54,6 +54,50 @@ cpdef ws2d(np.ndarray[dtype_t] y, float lmda, np.ndarray[dtype_t] w):
     return z
 
 
+cdef ws2d_internal(np.ndarray[dtype_t] y, float lmda, array[float] w):
+    cdef array flt_array_template = array('f', [])
+    cdef int i, i1, i2, m, n
+    cdef array z, d, c, e
+
+    n = y.shape[0]
+    m = n - 1
+
+    z = clone(flt_array_template,n,zero=False)
+    d = clone(flt_array_template,n,zero=False)
+    c = clone(flt_array_template,n,zero=False)
+    e = clone(flt_array_template,n,zero=False)
+
+    d.data.as_floats[0] = w.data.as_floats[0] + lmda
+    c.data.as_floats[0] = (-2 * lmda) / d.data.as_floats[0]
+    e.data.as_floats[0] = lmda /d.data.as_floats[0]
+    z.data.as_floats[0] = w.data.as_floats[0] * y[0]
+    d.data.as_floats[1] = w.data.as_floats[1] + 5 * lmda - d.data.as_floats[0] * (c.data.as_floats[0] * c.data.as_floats[0])
+    c.data.as_floats[1] = (-4 * lmda - d.data.as_floats[0] * c.data.as_floats[0] * e.data.as_floats[0]) / d.data.as_floats[1]
+    e.data.as_floats[1] =  lmda / d.data.as_floats[1]
+    z.data.as_floats[1] = w.data.as_floats[1] * y[1] - c.data.as_floats[0] * z.data.as_floats[0]
+    for i in range(2,m-1):
+        i1 = i - 1
+        i2 = i - 2
+        d.data.as_floats[i]= w.data.as_floats[i] + 6 *  lmda - (c.data.as_floats[i1] * c.data.as_floats[i1]) * d.data.as_floats[i1] - (e.data.as_floats[i2] * e.data.as_floats[i2]) * d.data.as_floats[i2]
+        c.data.as_floats[i] = (-4 *  lmda - d.data.as_floats[i1] * c.data.as_floats[i1] * e.data.as_floats[i1])/ d.data.as_floats[i]
+        e.data.as_floats[i] =  lmda / d.data.as_floats[i]
+        z.data.as_floats[i] = w.data.as_floats[i] * y[i] - c.data.as_floats[i1] * z.data.as_floats[i1] - e.data.as_floats[i2] * z.data.as_floats[i2]
+    i1 = m - 2
+    i2 = m - 3
+    d.data.as_floats[m - 1] = w.data.as_floats[m - 1] + 5 *  lmda - (c.data.as_floats[i1] * c.data.as_floats[i1]) * d.data.as_floats[i1] - (e.data.as_floats[i2] * e.data.as_floats[i2]) * d.data.as_floats[i2]
+    c.data.as_floats[m - 1] = (-2 *  lmda - d.data.as_floats[i1] * c.data.as_floats[i1] * e.data.as_floats[i1]) / d.data.as_floats[m - 1]
+    z.data.as_floats[m - 1] = w.data.as_floats[m - 1] * y[m - 1] - c.data.as_floats[i1] * z.data.as_floats[i1] - e.data.as_floats[i2] * z.data.as_floats[i2]
+    i1 = m - 1
+    i2 = m - 2
+    d.data.as_floats[m] = w.data.as_floats[m] +  lmda - (c.data.as_floats[i1] * c.data.as_floats[i1]) * d.data.as_floats[i1] - (e.data.as_floats[i2] * e.data.as_floats[i2]) * d.data.as_floats[i2]
+    z.data.as_floats[m] = (w.data.as_floats[m] * y[m] - c.data.as_floats[i1] * z.data.as_floats[i1] - e.data.as_floats[i2] * z.data.as_floats[i2]) / d.data.as_floats[m]
+    z.data.as_floats[m - 1] = z.data.as_floats[m - 1] / d.data.as_floats[m - 1] - c.data.as_floats[m - 1] * z.data.as_floats[m]
+    for i in range(m-2,-1,-1):
+        z.data.as_floats[i] = z.data.as_floats[i] / d.data.as_floats[i] - c.data.as_floats[i] * z.data.as_floats[i + 1] - e.data.as_floats[i] * z.data.as_floats[i + 2]
+
+    return z
+
+
 cpdef ws2d_vc(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] llas):
 
     cdef array template = array('f', [])
@@ -62,7 +106,6 @@ cpdef ws2d_vc(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] llas):
     cdef int m, m1, m2, nl, nl1, lix, i, k
     cdef float w_tmp, y_tmp, z_tmp, z2, llastep, f1, f2, p1, p2, l, l1, l2, vmin, lopt
 
-    #m = len(y)
     m = y.shape[0]
     m1 = m - 1
     m2 = m - 2
@@ -85,11 +128,8 @@ cpdef ws2d_vc(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] llas):
     for lix in range(nl):
         l = pow(10,llas.data.as_floats[lix])
         z[0:m] = ws2d(y,l,w)
-        ### yellow lines means data.as_floats not from array oject
         for i in range(m):
-            #w_tmp = w.data.as_floats[i]
             w_tmp = w[i]
-            #y_tmp = y.data.as_floats[i]
             y_tmp = y[i]
             z_tmp = z.data.as_floats[i]
             fits.data.as_floats[lix] += pow(w_tmp * (y_tmp - z_tmp),2)
