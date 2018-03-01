1ded12f0286a87a390c81d476138bae3fdd59f5d
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2018-03-01 16:41:47 +0100

    initial commit CLI functions

---
+++ scripts/processMODIS.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def main():
+    pass
+
+    # ppatt = re.compile(r'M\w{6}')
+    # tpatt = re.compile(r'h\d+v\d+\.\d{3}')
+    # grps = list(set(['-'.join(re.findall(ppatt,os.path.basename(x)) + re.findall(tpatt,os.path.basename(x)))  for x in fls]))
+    # fls_sub = [x for x in fls if g.split('-')[0] in x and g.split('-')[1] in x]
+
+
+if __name__ == '__main__':
+    main()
