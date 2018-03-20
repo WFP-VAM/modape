914519c13bc639c4d9552cf7d139c2a0e2e38d8b
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2018-03-20 15:36:26 +0100

    add LDOM

--- wsmtk/utils.py
+++ wsmtk/utils.py
@@ -9,3 +9,14 @@ def block_view(A, block= (3, 3)):
     shape= (A.shape[0]// block[0], A.shape[1]// block[1])+ block
     strides= (block[0]* A.strides[0], block[1]* A.strides[1])+ A.strides
     return ast(A, shape= shape, strides= strides)
+
+def LDOM(x):
+    '''get last day of month'''
+    yr = x.year
+    mn = x.month
+    if mn is 12:
+        mn = 1
+        yr +=1
+    else:
+        mn += 1
+    return(datetime.date(yr,mn,1) - datetime.timedelta(days=1))
