fe103e1e5d20f6399bae68b1c79b6dfafb52572e
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2018-04-27 17:24:12 +0200

    add SessionWithHeaderRedirection to utils

--- wsmtk/utils.py
+++ wsmtk/utils.py
@@ -1,5 +1,6 @@
 from numpy.lib.stride_tricks import as_strided as ast
 import datetime
+import requests
 
 def block_view(A, block= (3, 3)):
     ## Credit to http://stackoverflow.com/a/5078155/1828289
@@ -35,3 +36,48 @@ def aoi2ix(ref,aoi,res):
     yd = int(round((isect[1] - isect[3])/res))#+1
 
     return((xoff,xd,yoff,yd))
+
+
+## from https://wiki.earthdata.nasa.gov/display/EL/How+To+Access+Data+With+Python
+
+# overriding requests.Session.rebuild_auth to mantain headers when redirected
+
+class SessionWithHeaderRedirection(requests.Session):
+
+    AUTH_HOST = 'urs.earthdata.nasa.gov'
+
+    def __init__(self, username, password):
+
+        super(SessionWithHeaderRedirection,self).__init__()
+
+        self.auth = (username, password)
+
+
+
+
+   # Overrides from the library to keep headers when redirected to or from
+
+   # the NASA auth host.
+
+    def rebuild_auth(self, prepared_request, response):
+
+        headers = prepared_request.headers
+
+        url = prepared_request.url
+
+
+
+        if 'Authorization' in headers:
+
+            original_parsed = requests.utils.urlparse(response.request.url)
+
+            redirect_parsed = requests.utils.urlparse(url)
+
+
+
+            if (original_parsed.hostname != redirect_parsed.hostname) and redirect_parsed.hostname != self.AUTH_HOST and original_parsed.hostname != self.AUTH_HOST:
+                del headers['Authorization']
+
+
+
+        return
