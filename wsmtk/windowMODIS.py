1325cc9d29265ba0f3ab33fa35cf3e6c63a9fd6b
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2018-07-11 13:59:21 +0200

    modify input, make ROI optionable

--- wsmtk/windowMODIS.py
+++ wsmtk/windowMODIS.py
@@ -15,9 +15,9 @@ import pickle
 def main():
 
     parser = argparse.ArgumentParser(description="Extract a window from MODIS products")
-    parser.add_argument("product", help='MODIS product ID')
-    parser.add_argument("--roi", help='Region of interest. Can be LAT/LON point or bounding box in format llx lly urx ury',nargs='+',required=True,type=float)
-    parser.add_argument("--prcdir", help='Storage directory for PROCESSED MODIS files',default=os.getcwd(),metavar='')
+    parser.add_argument("path", help='Path to processed MODIS h5 files')
+    parser.add_argument("-p","--product", help='MODIS product ID (can be parial match with *)',metavar='')
+    parser.add_argument("--roi", help='Region of interest. Can be LAT/LON point or bounding box in format llx lly urx ury',nargs='+',type=float)
     parser.add_argument("--region", help='region 3 letter region code (default is "reg")',default='reg',metavar='')
     parser.add_argument("-b","--begin-date", help='Start date (YYYYMM)',default=datetime.date(2000,1,1).strftime("%Y%m"),metavar='')
     parser.add_argument("-e","--end-date", help='End date (YYYYMM)',default=datetime.date.today().strftime("%Y%m"),metavar='')
@@ -32,10 +32,43 @@ def main():
 
     args = parser.parse_args()
 
+    if not os.path.exists(args.path):
+        raise SystemExit('directory PATH does not exist!')
+
+    if args.product:
+        # force product code upper
+        args.product = args.product.upper()
+    else:
+        args.product = ''
+
+    if args.lgrid:
+        dset = 'lgrid'
+    else:
+        dset = 'data'
+
+    if args.roi and len(args.roi) == 4:
+        # change order or corner coordinates for MODIStiles
+        args.roi = [args.roi[i] for i in [0,3,2,1]]
+
     this_dir, this_filename = os.path.split(__file__)
 
     with open(os.path.join(this_dir, "data", "MODIS_V6_PT.pkl"),'rb') as table_raw:
         product_table = pickle.load(table_raw)
+
+    h5files = glob.glob('{}/{}*h5'.format(args.path,args.product))
+
+    if len(h5files) > 0:
+        if len(set([re.sub('h\d{2}v\d{2}.','',os.path.basename(x)) for x in h5files])) > 1:
+            raise SystemExit("\nMultiple product types found! Please specify and/or check product parameter!\n")
+    else:
+        raise SystemExit('\nNo products found in specified path (for specified product) - please check input!\n')
+
+    product_ = re.sub('(M\w{6}).+','\\1', os.path.basename(h5files[0]))
+
+    if 'MXD' in product_:
+        global_flag = int(product_table[re.sub('MXD','MOD',product_)]['pixel_size']) == 5600
+    else:
+        global_flag = int(product_table[product_]['pixel_size']) == 5600
 
     # check if targetdir exists
     if not os.path.exists(args.targetdir):
@@ -46,28 +79,7 @@ def main():
         except:
             raise
 
-    # force product code upper
-    args.product = args.product.upper()
-
-    if 'MXD' in args.product:
-        global_flag = int(product_table[re.sub('MXD','MOD',args.product)]['pixel_size']) == 5600
-    else:
-        global_flag = int(product_table[args.product]['pixel_size']) == 5600
-
-    if args.lgrid:
-        dset = 'lgrid'
-    else:
-        dset = 'data'
-
-    # change order or corner coordinates for MODIStiles
-    if len(args.roi) is 4:
-        args.roi = [args.roi[i] for i in [0,3,2,1]]
-
-    if global_flag:
-
-        h5files = glob.glob(args.prcdir + '/*h5')
-
-    else:
+    if not global_flag and args.roi:
 
         tiles = MODIStiles(args.roi)
 
@@ -77,27 +89,24 @@ def main():
         tileRX = re.compile('|'.join(tiles.tiles))
 
         # files for tile result
-        h5files = [y for x in os.walk(args.prcdir) for y in glob.glob(os.path.join(x[0], '*.h5')) if re.search(tileRX,y)]
+        h5files = [x for x in h5files if re.search(tileRX,x)]
 
-    # filter for product (and parameter)
+    # filter for parameter
     if args.parameter:
-        h5files_fil = [x for x in h5files if args.product in x and args.parameter in x]
-    else:
-        h5files_fil = [x for x in h5files if args.product in x]
+        h5files = [x for x in h5files if args.parameter in x]
 
-    if (len(h5files_fil)) is 0:
-        raise SystemExit("\nNo processed MODIS HDF5 files found for combination of product/tile (and parameter)")
+    assert len(h5files) > 0, "\nNo processed MODIS HDF5 files found for combination of product/tile (and parameter)"
 
     # loop over parameters (could be multiple if unspecified)
 
-    for par in set([re.sub('.+([^\W\d_]{3}).h5','\\1',x) for x in h5files_fil]):
+    for par in set([re.sub('.+([^\W\d_]{3}).h5','\\1',x) for x in h5files]):
 
         print('\n')
 
-        h5files_fil_par = [x for x in h5files_fil if par in x]
+        h5files_par = [x for x in h5files if par in x]
 
         # get mosaic
-        mosaic = MODISmosaic(files=h5files_fil_par,datemin=args.begin_date,datemax=args.end_date,global_flag=global_flag)
+        mosaic = MODISmosaic(files=h5files_par,datemin=args.begin_date,datemax=args.end_date,global_flag=global_flag)
 
         if args.lgrid:
 
@@ -109,7 +118,7 @@ def main():
 
                 try:
 
-                    if len(args.roi) > 2:
+                    if args.roi and len(args.roi) > 2:
                         ds = gdal.Warp(filename,mosaic_ropen.raster,
                         dstSRS='EPSG:4326',
                         outputType=mosaic_ropen.dt_gdal[0],
@@ -151,7 +160,7 @@ def main():
 
                     try:
 
-                        if len(args.roi) > 2:
+                        if args.roi and len(args.roi) > 2:
 
                             ds = gdal.Warp(filename,mosaic_ropen.raster,
                             dstSRS='EPSG:4326',
