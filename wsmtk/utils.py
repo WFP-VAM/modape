2c882759eda21bb16ccad19605ba1b0bd047be14
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2018-07-30 16:14:56 +0200

    add docu for utils

--- wsmtk/utils.py
+++ wsmtk/utils.py
@@ -8,7 +8,17 @@ import multiprocessing
 from .whittaker import ws2d, ws2d_vc, ws2d_vc_asy
 
 def dtype_GDNP(dt):
-    '''GDAL/NP DataType helper'''
+    '''GDAL/NP DataType helper.
+
+    Parses datatype in str or GDAL int.
+
+    Agrs:
+        dt (str/int): DataType
+
+    Returns:
+        Tuple with DataType as GDAL integer and string
+    '''
+
     dt_dict={
     1: 'uint8',
     2: 'uint16',
@@ -33,7 +43,16 @@ def block_view(A, block= (3, 3)):
     return ast(A, shape= shape, strides= strides)
 
 def LDOM(x):
-    '''get last day of month'''
+    '''Get last day of month.
+
+    Args:
+        x (datetime): Input date
+
+    Returns:
+        Datetime object
+    '''
+
+
     yr = x.year
     mn = x.month
     if mn is 12:
@@ -45,8 +64,7 @@ def LDOM(x):
 
 
 def aoi2ix(ref,aoi,res):
-
-    '''Extract indices for intersection over reference'''
+    '''Extract indices for intersection over reference.'''
 
     isect = [max(ref[0],aoi[0]),min(ref[1],aoi[1]),min(ref[2],aoi[2]),max(ref[3],aoi[3])]
 
@@ -59,20 +77,27 @@ def aoi2ix(ref,aoi,res):
     return((xoff,xd,yoff,yd))
 
 
-## from https://wiki.earthdata.nasa.gov/display/EL/How+To+Access+Data+With+Python
-
-# overriding requests.Session.rebuild_auth to mantain headers when redirected
 
 class SessionWithHeaderRedirection(requests.Session):
+    ''' Session class for MODIS query.
+
+    Overriding requests.Session.rebuild_auth to mantain headers when redirected.
+    From https://wiki.earthdata.nasa.gov/display/EL/How+To+Access+Data+With+Python
+    '''
 
     AUTH_HOST = 'urs.earthdata.nasa.gov'
 
     def __init__(self, username, password):
+        '''Create SessionWithHeaderRedirection instance.
+
+        Args:
+            username (str): Earthdata username
+            password (str): Earthdata password
+        '''
 
         super(SessionWithHeaderRedirection,self).__init__()
 
         self.auth = (username, password)
-
 
    # Overrides from the library to keep headers when redirected to or from
 
@@ -84,15 +109,11 @@ class SessionWithHeaderRedirection(requests.Session):
 
         url = prepared_request.url
 
-
-
         if 'Authorization' in headers:
 
             original_parsed = requests.utils.urlparse(response.request.url)
 
             redirect_parsed = requests.utils.urlparse(url)
-
-
 
             if (original_parsed.hostname != redirect_parsed.hostname) and redirect_parsed.hostname != self.AUTH_HOST and original_parsed.hostname != self.AUTH_HOST:
                 del headers['Authorization']
@@ -100,6 +121,8 @@ class SessionWithHeaderRedirection(requests.Session):
         return
 
 def txx(x):
+    '''Converts tempint integer to flag.'''
+
     if x:
         if int(x) == 5:
             return 'p'
@@ -112,20 +135,24 @@ def txx(x):
 
 
 def fromjulian(x):
+    '''Parses julian date string to datetime object.'''
+
     return datetime.datetime.strptime(x,'%Y%j').date()
 
 def init_shared(ncell):
-    '''Create shared value array for smoothing'''
+    '''Create shared value array for smoothing.'''
     shared_array_base = multiprocessing.Array(ctypes.c_float,ncell,lock=False)
     return(shared_array_base)
 
 def tonumpyarray(shared_array):
-
+    '''Create numpy array from shared memory.'''
     nparray= np.frombuffer(shared_array,dtype=ctypes.c_float)
     assert nparray.base is shared_array
     return nparray
 
 def init_parameters(**kwargs):
+    '''Initialize parameters for smoothing in workers.'''
+
     params = dict(zip(['shared_array','shared_sarr','nd','s','srange','p','dim'],[None for x in range(7)]))
 
     for key, value in kwargs.items():
@@ -133,6 +160,13 @@ def init_parameters(**kwargs):
     return params
 
 def init_worker(shared_array_,parameters_):
+    '''Initialize worker for smoothing.
+
+    Args:
+        shared_array_: Object returned by init_shared
+        parameters_: Dictionary returned by init_parameters
+    '''
+
     global shared_array
     global nd
     global s
@@ -154,7 +188,11 @@ def init_worker(shared_array_,parameters_):
 
 
 def execute_ws2d(ix):
-    #worker function for parallel smoothing using whittaker 2d with fixed lambda
+    '''Execute whittaker smoother with fixed s in worker.
+
+    Args:
+        ix ([int]): List of indices as integer
+    '''
     arr = tonumpyarray(shared_array)
     arr.shape = dim
     for ii in ix:
@@ -162,7 +200,7 @@ def execute_ws2d(ix):
             arr[ii,] = ws2d(y = arr[ii,], lmda = s, w = np.array((arr[ii,] != nd) * 1,dtype='float32'))
 
 def execute_ws2d_sgrid(ix):
-    #worker function for parallel smoothing using whittaker 2d with existing lambda grid
+    '''Execute whittaker smoother with s from grid in worker.'''
     arr = tonumpyarray(shared_array)
     sarr = tonumpyarray(shared_sarr)
     arr.shape = dim
@@ -172,7 +210,7 @@ def execute_ws2d_sgrid(ix):
             arr[ii,] = ws2d(y = arr[ii,], lmda = 10**sarr[ii], w = np.array((arr[ii,] != nd ) * 1,dtype='float32'))
 
 def execute_ws2d_vc(ix):
-    #worker function for parallel smoothing using whittaker 2d with v-curve optimization
+    '''Execute whittaker smoother with V-curve optimization of s in worker.'''
     arr = tonumpyarray(shared_array)
     sarr = tonumpyarray(shared_sarr)
     arr.shape = dim
@@ -182,7 +220,7 @@ def execute_ws2d_vc(ix):
             arr[ii,], sarr[ii] =  ws2d_vc(y = arr[ii], w = np.array((arr[ii,] != nd) * 1,dtype='float32'), llas = srange)
 
 def execute_ws2d_vc_asy(ix):
-    #worker function for parallel asymmetric smoothing using whittaker 2d with v-curve optimization
+    '''Execute asymmetric whittaker smoother with V-curve optimization of s in worker.'''
     arr = tonumpyarray(shared_array)
     sarr = tonumpyarray(shared_sarr)
     arr.shape = dim
