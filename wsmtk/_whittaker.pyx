7fc6788cbc01a83645eda00106e09ca6769709e1
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2019-01-11 11:49:10 +0100

    change datatype in whittaker functions to double

--- wsmtk/_whittaker.pyx
+++ wsmtk/_whittaker.pyx
@@ -5,106 +5,106 @@ from cpython.array cimport array, clone
 
 cimport numpy as np
 import numpy as np
-tFloat = np.float32
-ctypedef np.float32_t dtype_t
+tFloat = np.double
+ctypedef np.double_t dtype_t
 from libc.math cimport log, pow, sqrt
 
-cpdef ws2d(np.ndarray[dtype_t] y, float lmda, np.ndarray[dtype_t] w):
-    cdef array flt_array_template = array('f', [])
+cpdef ws2d(np.ndarray[dtype_t] y, double lmda, np.ndarray[dtype_t] w):
+    cdef array dbl_array_template = array('d', [])
     cdef int i, i1, i2, m, n
     cdef array z, d, c, e
 
     n = y.shape[0]
     m = n - 1
 
-    z = clone(flt_array_template,n,zero=False)
-    d = clone(flt_array_template,n,zero=False)
-    c = clone(flt_array_template,n,zero=False)
-    e = clone(flt_array_template,n,zero=False)
+    z = clone(dbl_array_template,n,zero=False)
+    d = clone(dbl_array_template,n,zero=False)
+    c = clone(dbl_array_template,n,zero=False)
+    e = clone(dbl_array_template,n,zero=False)
 
-    d.data.as_floats[0] = w[0] + lmda
-    c.data.as_floats[0] = (-2 * lmda) / d.data.as_floats[0]
-    e.data.as_floats[0] = lmda /d.data.as_floats[0]
-    z.data.as_floats[0] = w[0] * y[0]
-    d.data.as_floats[1] = w[1] + 5 * lmda - d.data.as_floats[0] * (c.data.as_floats[0] * c.data.as_floats[0])
-    c.data.as_floats[1] = (-4 * lmda - d.data.as_floats[0] * c.data.as_floats[0] * e.data.as_floats[0]) / d.data.as_floats[1]
-    e.data.as_floats[1] =  lmda / d.data.as_floats[1]
-    z.data.as_floats[1] = w[1] * y[1] - c.data.as_floats[0] * z.data.as_floats[0]
+    d.data.as_doubles[0] = w[0] + lmda
+    c.data.as_doubles[0] = (-2 * lmda) / d.data.as_doubles[0]
+    e.data.as_doubles[0] = lmda /d.data.as_doubles[0]
+    z.data.as_doubles[0] = w[0] * y[0]
+    d.data.as_doubles[1] = w[1] + 5 * lmda - d.data.as_doubles[0] * (c.data.as_doubles[0] * c.data.as_doubles[0])
+    c.data.as_doubles[1] = (-4 * lmda - d.data.as_doubles[0] * c.data.as_doubles[0] * e.data.as_doubles[0]) / d.data.as_doubles[1]
+    e.data.as_doubles[1] =  lmda / d.data.as_doubles[1]
+    z.data.as_doubles[1] = w[1] * y[1] - c.data.as_doubles[0] * z.data.as_doubles[0]
     for i in range(2,m-1):
         i1 = i - 1
         i2 = i - 2
-        d.data.as_floats[i]= w[i] + 6 *  lmda - (c.data.as_floats[i1] * c.data.as_floats[i1]) * d.data.as_floats[i1] - (e.data.as_floats[i2] * e.data.as_floats[i2]) * d.data.as_floats[i2]
-        c.data.as_floats[i] = (-4 *  lmda - d.data.as_floats[i1] * c.data.as_floats[i1] * e.data.as_floats[i1])/ d.data.as_floats[i]
-        e.data.as_floats[i] =  lmda / d.data.as_floats[i]
-        z.data.as_floats[i] = w[i] * y[i] - c.data.as_floats[i1] * z.data.as_floats[i1] - e.data.as_floats[i2] * z.data.as_floats[i2]
+        d.data.as_doubles[i]= w[i] + 6 *  lmda - (c.data.as_doubles[i1] * c.data.as_doubles[i1]) * d.data.as_doubles[i1] - (e.data.as_doubles[i2] * e.data.as_doubles[i2]) * d.data.as_doubles[i2]
+        c.data.as_doubles[i] = (-4 *  lmda - d.data.as_doubles[i1] * c.data.as_doubles[i1] * e.data.as_doubles[i1])/ d.data.as_doubles[i]
+        e.data.as_doubles[i] =  lmda / d.data.as_doubles[i]
+        z.data.as_doubles[i] = w[i] * y[i] - c.data.as_doubles[i1] * z.data.as_doubles[i1] - e.data.as_doubles[i2] * z.data.as_doubles[i2]
     i1 = m - 2
     i2 = m - 3
-    d.data.as_floats[m - 1] = w[m - 1] + 5 *  lmda - (c.data.as_floats[i1] * c.data.as_floats[i1]) * d.data.as_floats[i1] - (e.data.as_floats[i2] * e.data.as_floats[i2]) * d.data.as_floats[i2]
-    c.data.as_floats[m - 1] = (-2 *  lmda - d.data.as_floats[i1] * c.data.as_floats[i1] * e.data.as_floats[i1]) / d.data.as_floats[m - 1]
-    z.data.as_floats[m - 1] = w[m - 1] * y[m - 1] - c.data.as_floats[i1] * z.data.as_floats[i1] - e.data.as_floats[i2] * z.data.as_floats[i2]
+    d.data.as_doubles[m - 1] = w[m - 1] + 5 *  lmda - (c.data.as_doubles[i1] * c.data.as_doubles[i1]) * d.data.as_doubles[i1] - (e.data.as_doubles[i2] * e.data.as_doubles[i2]) * d.data.as_doubles[i2]
+    c.data.as_doubles[m - 1] = (-2 *  lmda - d.data.as_doubles[i1] * c.data.as_doubles[i1] * e.data.as_doubles[i1]) / d.data.as_doubles[m - 1]
+    z.data.as_doubles[m - 1] = w[m - 1] * y[m - 1] - c.data.as_doubles[i1] * z.data.as_doubles[i1] - e.data.as_doubles[i2] * z.data.as_doubles[i2]
     i1 = m - 1
     i2 = m - 2
-    d.data.as_floats[m] = w[m] +  lmda - (c.data.as_floats[i1] * c.data.as_floats[i1]) * d.data.as_floats[i1] - (e.data.as_floats[i2] * e.data.as_floats[i2]) * d.data.as_floats[i2]
-    z.data.as_floats[m] = (w[m] * y[m] - c.data.as_floats[i1] * z.data.as_floats[i1] - e.data.as_floats[i2] * z.data.as_floats[i2]) / d.data.as_floats[m]
-    z.data.as_floats[m - 1] = z.data.as_floats[m - 1] / d.data.as_floats[m - 1] - c.data.as_floats[m - 1] * z.data.as_floats[m]
+    d.data.as_doubles[m] = w[m] +  lmda - (c.data.as_doubles[i1] * c.data.as_doubles[i1]) * d.data.as_doubles[i1] - (e.data.as_doubles[i2] * e.data.as_doubles[i2]) * d.data.as_doubles[i2]
+    z.data.as_doubles[m] = (w[m] * y[m] - c.data.as_doubles[i1] * z.data.as_doubles[i1] - e.data.as_doubles[i2] * z.data.as_doubles[i2]) / d.data.as_doubles[m]
+    z.data.as_doubles[m - 1] = z.data.as_doubles[m - 1] / d.data.as_doubles[m - 1] - c.data.as_doubles[m - 1] * z.data.as_doubles[m]
     for i in range(m-2,-1,-1):
-        z.data.as_floats[i] = z.data.as_floats[i] / d.data.as_floats[i] - c.data.as_floats[i] * z.data.as_floats[i + 1] - e.data.as_floats[i] * z.data.as_floats[i + 2]
+        z.data.as_doubles[i] = z.data.as_doubles[i] / d.data.as_doubles[i] - c.data.as_doubles[i] * z.data.as_doubles[i + 1] - e.data.as_doubles[i] * z.data.as_doubles[i + 2]
 
     return z
 
 
-cdef ws2d_internal(np.ndarray[dtype_t] y, float lmda, array[float] w):
+cdef ws2d_internal(np.ndarray[dtype_t] y, double lmda, array[double] w):
     ## internal whittaker function for asymmetric smoothing
-    cdef array flt_array_template = array('f', [])
+    cdef array dbl_array_template = array('d', [])
     cdef int i, i1, i2, m, n
     cdef array z, d, c, e
 
     n = y.shape[0]
     m = n - 1
 
-    z = clone(flt_array_template,n,zero=False)
-    d = clone(flt_array_template,n,zero=False)
-    c = clone(flt_array_template,n,zero=False)
-    e = clone(flt_array_template,n,zero=False)
+    z = clone(dbl_array_template,n,zero=False)
+    d = clone(dbl_array_template,n,zero=False)
+    c = clone(dbl_array_template,n,zero=False)
+    e = clone(dbl_array_template,n,zero=False)
 
-    d.data.as_floats[0] = w.data.as_floats[0] + lmda
-    c.data.as_floats[0] = (-2 * lmda) / d.data.as_floats[0]
-    e.data.as_floats[0] = lmda /d.data.as_floats[0]
-    z.data.as_floats[0] = w.data.as_floats[0] * y[0]
-    d.data.as_floats[1] = w.data.as_floats[1] + 5 * lmda - d.data.as_floats[0] * (c.data.as_floats[0] * c.data.as_floats[0])
-    c.data.as_floats[1] = (-4 * lmda - d.data.as_floats[0] * c.data.as_floats[0] * e.data.as_floats[0]) / d.data.as_floats[1]
-    e.data.as_floats[1] =  lmda / d.data.as_floats[1]
-    z.data.as_floats[1] = w.data.as_floats[1] * y[1] - c.data.as_floats[0] * z.data.as_floats[0]
+    d.data.as_doubles[0] = w.data.as_doubles[0] + lmda
+    c.data.as_doubles[0] = (-2 * lmda) / d.data.as_doubles[0]
+    e.data.as_doubles[0] = lmda /d.data.as_doubles[0]
+    z.data.as_doubles[0] = w.data.as_doubles[0] * y[0]
+    d.data.as_doubles[1] = w.data.as_doubles[1] + 5 * lmda - d.data.as_doubles[0] * (c.data.as_doubles[0] * c.data.as_doubles[0])
+    c.data.as_doubles[1] = (-4 * lmda - d.data.as_doubles[0] * c.data.as_doubles[0] * e.data.as_doubles[0]) / d.data.as_doubles[1]
+    e.data.as_doubles[1] =  lmda / d.data.as_doubles[1]
+    z.data.as_doubles[1] = w.data.as_doubles[1] * y[1] - c.data.as_doubles[0] * z.data.as_doubles[0]
     for i in range(2,m-1):
         i1 = i - 1
         i2 = i - 2
-        d.data.as_floats[i]= w.data.as_floats[i] + 6 *  lmda - (c.data.as_floats[i1] * c.data.as_floats[i1]) * d.data.as_floats[i1] - (e.data.as_floats[i2] * e.data.as_floats[i2]) * d.data.as_floats[i2]
-        c.data.as_floats[i] = (-4 *  lmda - d.data.as_floats[i1] * c.data.as_floats[i1] * e.data.as_floats[i1])/ d.data.as_floats[i]
-        e.data.as_floats[i] =  lmda / d.data.as_floats[i]
-        z.data.as_floats[i] = w.data.as_floats[i] * y[i] - c.data.as_floats[i1] * z.data.as_floats[i1] - e.data.as_floats[i2] * z.data.as_floats[i2]
+        d.data.as_doubles[i]= w.data.as_doubles[i] + 6 *  lmda - (c.data.as_doubles[i1] * c.data.as_doubles[i1]) * d.data.as_doubles[i1] - (e.data.as_doubles[i2] * e.data.as_doubles[i2]) * d.data.as_doubles[i2]
+        c.data.as_doubles[i] = (-4 *  lmda - d.data.as_doubles[i1] * c.data.as_doubles[i1] * e.data.as_doubles[i1])/ d.data.as_doubles[i]
+        e.data.as_doubles[i] =  lmda / d.data.as_doubles[i]
+        z.data.as_doubles[i] = w.data.as_doubles[i] * y[i] - c.data.as_doubles[i1] * z.data.as_doubles[i1] - e.data.as_doubles[i2] * z.data.as_doubles[i2]
     i1 = m - 2
     i2 = m - 3
-    d.data.as_floats[m - 1] = w.data.as_floats[m - 1] + 5 *  lmda - (c.data.as_floats[i1] * c.data.as_floats[i1]) * d.data.as_floats[i1] - (e.data.as_floats[i2] * e.data.as_floats[i2]) * d.data.as_floats[i2]
-    c.data.as_floats[m - 1] = (-2 *  lmda - d.data.as_floats[i1] * c.data.as_floats[i1] * e.data.as_floats[i1]) / d.data.as_floats[m - 1]
-    z.data.as_floats[m - 1] = w.data.as_floats[m - 1] * y[m - 1] - c.data.as_floats[i1] * z.data.as_floats[i1] - e.data.as_floats[i2] * z.data.as_floats[i2]
+    d.data.as_doubles[m - 1] = w.data.as_doubles[m - 1] + 5 *  lmda - (c.data.as_doubles[i1] * c.data.as_doubles[i1]) * d.data.as_doubles[i1] - (e.data.as_doubles[i2] * e.data.as_doubles[i2]) * d.data.as_doubles[i2]
+    c.data.as_doubles[m - 1] = (-2 *  lmda - d.data.as_doubles[i1] * c.data.as_doubles[i1] * e.data.as_doubles[i1]) / d.data.as_doubles[m - 1]
+    z.data.as_doubles[m - 1] = w.data.as_doubles[m - 1] * y[m - 1] - c.data.as_doubles[i1] * z.data.as_doubles[i1] - e.data.as_doubles[i2] * z.data.as_doubles[i2]
     i1 = m - 1
     i2 = m - 2
-    d.data.as_floats[m] = w.data.as_floats[m] +  lmda - (c.data.as_floats[i1] * c.data.as_floats[i1]) * d.data.as_floats[i1] - (e.data.as_floats[i2] * e.data.as_floats[i2]) * d.data.as_floats[i2]
-    z.data.as_floats[m] = (w.data.as_floats[m] * y[m] - c.data.as_floats[i1] * z.data.as_floats[i1] - e.data.as_floats[i2] * z.data.as_floats[i2]) / d.data.as_floats[m]
-    z.data.as_floats[m - 1] = z.data.as_floats[m - 1] / d.data.as_floats[m - 1] - c.data.as_floats[m - 1] * z.data.as_floats[m]
+    d.data.as_doubles[m] = w.data.as_doubles[m] +  lmda - (c.data.as_doubles[i1] * c.data.as_doubles[i1]) * d.data.as_doubles[i1] - (e.data.as_doubles[i2] * e.data.as_doubles[i2]) * d.data.as_doubles[i2]
+    z.data.as_doubles[m] = (w.data.as_doubles[m] * y[m] - c.data.as_doubles[i1] * z.data.as_doubles[i1] - e.data.as_doubles[i2] * z.data.as_doubles[i2]) / d.data.as_doubles[m]
+    z.data.as_doubles[m - 1] = z.data.as_doubles[m - 1] / d.data.as_doubles[m - 1] - c.data.as_doubles[m - 1] * z.data.as_doubles[m]
     for i in range(m-2,-1,-1):
-        z.data.as_floats[i] = z.data.as_floats[i] / d.data.as_floats[i] - c.data.as_floats[i] * z.data.as_floats[i + 1] - e.data.as_floats[i] * z.data.as_floats[i + 2]
+        z.data.as_doubles[i] = z.data.as_doubles[i] / d.data.as_doubles[i] - c.data.as_doubles[i] * z.data.as_doubles[i + 1] - e.data.as_doubles[i] * z.data.as_doubles[i + 2]
 
     return z
 
 
-cpdef ws2d_vc(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] llas):
+cpdef ws2d_vc(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[double] llas):
     ## vcurve
-    cdef array template = array('f', [])
+    cdef array template = array('d', [])
 
     cdef array fits, pens, diff1, lamids, v, z
     cdef int m, m1, m2, nl, nl1, lix, i, k
-    cdef float w_tmp, y_tmp, z_tmp, z2, llastep, f1, f2, p1, p2, l, l1, l2, vmin, lopt
+    cdef double w_tmp, y_tmp, z_tmp, z2, llastep, f1, f2, p1, p2, l, l1, l2, vmin, lopt
 
     m = y.shape[0]
     m1 = m - 1
@@ -114,7 +114,7 @@ cpdef ws2d_vc(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] llas):
     i = 0
     k = 0
 
-    template = array('f',[])
+    template = array('d',[])
 
     fits = clone(template, nl, True)
     pens = clone(template,nl,True)
@@ -126,24 +126,24 @@ cpdef ws2d_vc(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] llas):
     # Compute v-curve
 
     for lix in range(nl):
-        l = pow(10,llas.data.as_floats[lix])
+        l = pow(10,llas.data.as_doubles[lix])
         z[0:m] = ws2d(y,l,w)
         for i in range(m):
             w_tmp = w[i]
             y_tmp = y[i]
-            z_tmp = z.data.as_floats[i]
-            fits.data.as_floats[lix] += pow(w_tmp * (y_tmp - z_tmp),2)
-        fits.data.as_floats[lix] = log(fits.data.as_floats[lix])
+            z_tmp = z.data.as_doubles[i]
+            fits.data.as_doubles[lix] += pow(w_tmp * (y_tmp - z_tmp),2)
+        fits.data.as_doubles[lix] = log(fits.data.as_doubles[lix])
 
         for i in range(m1):
-            z_tmp = z.data.as_floats[i]
-            z2 = z.data.as_floats[i+1]
-            diff1.data.as_floats[i] = z2 - z_tmp
+            z_tmp = z.data.as_doubles[i]
+            z2 = z.data.as_doubles[i+1]
+            diff1.data.as_doubles[i] = z2 - z_tmp
         for i in range(m2):
-            z_tmp = diff1.data.as_floats[i]
-            z2 = diff1.data.as_floats[i+1]
-            pens.data.as_floats[lix] += pow(z2 - z_tmp,2)
-        pens.data.as_floats[lix] = log(pens.data.as_floats[lix])
+            z_tmp = diff1.data.as_doubles[i]
+            z2 = diff1.data.as_doubles[i+1]
+            pens.data.as_doubles[lix] += pow(z2 - z_tmp,2)
+        pens.data.as_doubles[lix] = log(pens.data.as_doubles[lix])
 
 
     # Construct v-curve
@@ -151,37 +151,37 @@ cpdef ws2d_vc(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] llas):
     llastep = llas[1] - llas[0]
 
     for i in range(nl1):
-        l1 = llas.data.as_floats[i]
-        l2 = llas.data.as_floats[i+1]
-        f1 = fits.data.as_floats[i]
-        f2 = fits.data.as_floats[i+1]
-        p1 = pens.data.as_floats[i]
-        p2 = pens.data.as_floats[i+1]
-        v.data.as_floats[i] = sqrt(pow(f2 - f1,2) + pow(p2 - p1,2)) / (log(10) * llastep)
-        lamids.data.as_floats[i] = (l1+l2) / 2
+        l1 = llas.data.as_doubles[i]
+        l2 = llas.data.as_doubles[i+1]
+        f1 = fits.data.as_doubles[i]
+        f2 = fits.data.as_doubles[i+1]
+        p1 = pens.data.as_doubles[i]
+        p2 = pens.data.as_doubles[i+1]
+        v.data.as_doubles[i] = sqrt(pow(f2 - f1,2) + pow(p2 - p1,2)) / (log(10) * llastep)
+        lamids.data.as_doubles[i] = (l1+l2) / 2
 
 
-    vmin = v.data.as_floats[k]
+    vmin = v.data.as_doubles[k]
     for i in range(1,nl1):
-        if v.data.as_floats[i] < vmin:
-            vmin = v.data.as_floats[i]
+        if v.data.as_doubles[i] < vmin:
+            vmin = v.data.as_doubles[i]
             k = i
 
-    lopt = pow(10,lamids.data.as_floats[k])
+    lopt = pow(10,lamids.data.as_doubles[k])
 
     z[0:m] = ws2d(y,lopt,w)
 
     return z, lopt
 
 
-cpdef ws2d_vc_asy(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] llas, float p):
+cpdef ws2d_vc_asy(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[double] llas, double p):
     ## vcurve with asymmetric smoothing
 
-    cdef array template = array('f', [])
+    cdef array template = array('d', [])
 
     cdef array fits, pens, diff1, lamids, v, z
     cdef int m, m1, m2, nl, nl1, lix, i, j, k
-    cdef float w_tmp, y_tmp, z_tmp, z2, llastep, fit1, fit2, pen1, pen2, l, l1, l2, vmin, lopt, p1
+    cdef double w_tmp, y_tmp, z_tmp, z2, llastep, fit1, fit2, pen1, pen2, l, l1, l2, vmin, lopt, p1
 
     m = y.shape[0]
     m1 = m - 1
@@ -193,7 +193,7 @@ cpdef ws2d_vc_asy(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] lla
     j = 0
     p1 = 1-p
 
-    template = array('f',[])
+    template = array('d',[])
 
     fits = clone(template, nl, True)
     pens = clone(template,nl,True)
@@ -208,24 +208,24 @@ cpdef ws2d_vc_asy(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] lla
     # Compute v-curve
 
     for lix in range(nl):
-        l = pow(10,llas.data.as_floats[lix])
+        l = pow(10,llas.data.as_doubles[lix])
 
         for i in range(10):
           for j in range(m):
             y_tmp = y[j]
-            z_tmp = z.data.as_floats[j]
+            z_tmp = z.data.as_doubles[j]
             if y_tmp > z_tmp:
-              wa.data.as_floats[j] = p
+              wa.data.as_doubles[j] = p
             else:
-              wa.data.as_floats[j] = p1
-            ww.data.as_floats[j] = w[j] * wa.data.as_floats[j]
+              wa.data.as_doubles[j] = p1
+            ww.data.as_doubles[j] = w[j] * wa.data.as_doubles[j]
 
           znew[0:m] = ws2d_internal(y,l,ww)
 
           z_tmp = 0.0
           j = 0
           for j in range(m):
-            z_tmp += abs(znew.data.as_floats[j] - z.data.as_floats[j])
+            z_tmp += abs(znew.data.as_doubles[j] - z.data.as_doubles[j])
 
           if z_tmp == 0.0:
             break
@@ -235,19 +235,19 @@ cpdef ws2d_vc_asy(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] lla
         for i in range(m):
             w_tmp = w[i]
             y_tmp = y[i]
-            z_tmp = z.data.as_floats[i]
-            fits.data.as_floats[lix] += pow(w_tmp * (y_tmp - z_tmp),2)
-        fits.data.as_floats[lix] = log(fits.data.as_floats[lix])
+            z_tmp = z.data.as_doubles[i]
+            fits.data.as_doubles[lix] += pow(w_tmp * (y_tmp - z_tmp),2)
+        fits.data.as_doubles[lix] = log(fits.data.as_doubles[lix])
 
         for i in range(m1):
-            z_tmp = z.data.as_floats[i]
-            z2 = z.data.as_floats[i+1]
-            diff1.data.as_floats[i] = z2 - z_tmp
+            z_tmp = z.data.as_doubles[i]
+            z2 = z.data.as_doubles[i+1]
+            diff1.data.as_doubles[i] = z2 - z_tmp
         for i in range(m2):
-            z_tmp = diff1.data.as_floats[i]
-            z2 = diff1.data.as_floats[i+1]
-            pens.data.as_floats[lix] += pow(z2 - z_tmp,2)
-        pens.data.as_floats[lix] = log(pens.data.as_floats[lix])
+            z_tmp = diff1.data.as_doubles[i]
+            z2 = diff1.data.as_doubles[i+1]
+            pens.data.as_doubles[lix] += pow(z2 - z_tmp,2)
+        pens.data.as_doubles[lix] = log(pens.data.as_doubles[lix])
 
 
     # Construct v-curve
@@ -255,24 +255,24 @@ cpdef ws2d_vc_asy(np.ndarray[dtype_t] y, np.ndarray[dtype_t] w, array[float] lla
     llastep = llas[1] - llas[0]
 
     for i in range(nl1):
-        l1 = llas.data.as_floats[i]
-        l2 = llas.data.as_floats[i+1]
-        fit1 = fits.data.as_floats[i]
-        fit2 = fits.data.as_floats[i+1]
-        pen1 = pens.data.as_floats[i]
-        pen2 = pens.data.as_floats[i+1]
-        v.data.as_floats[i] = sqrt(pow(fit2 - fit1,2) + pow(pen2 - pen1,2)) / (log(10) * llastep)
-        lamids.data.as_floats[i] = (l1+l2) / 2
+        l1 = llas.data.as_doubles[i]
+        l2 = llas.data.as_doubles[i+1]
+        fit1 = fits.data.as_doubles[i]
+        fit2 = fits.data.as_doubles[i+1]
+        pen1 = pens.data.as_doubles[i]
+        pen2 = pens.data.as_doubles[i+1]
+        v.data.as_doubles[i] = sqrt(pow(fit2 - fit1,2) + pow(pen2 - pen1,2)) / (log(10) * llastep)
+        lamids.data.as_doubles[i] = (l1+l2) / 2
 
 
-    vmin = v.data.as_floats[k]
+    vmin = v.data.as_doubles[k]
     for i in range(1,nl1):
-        if v.data.as_floats[i] < vmin:
-            vmin = v.data.as_floats[i]
+        if v.data.as_doubles[i] < vmin:
+            vmin = v.data.as_doubles[i]
             k = i
 
-    lopt = pow(10,lamids.data.as_floats[k])
+    lopt = pow(10,lamids.data.as_doubles[k])
 
     z[0:m] = ws2d_internal(y,lopt,ww)
 
-    return z, lopt, lamids, v
+    return z, lopt
