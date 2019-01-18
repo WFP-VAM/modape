e77258331389b3228fc50efb1adbdcf8e3439612
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2019-01-18 14:30:32 +0100

    split vc and vcp functionality

--- wsmtk/utils.py
+++ wsmtk/utils.py
@@ -6,7 +6,7 @@ import ctypes
 import multiprocessing
 import multiprocessing.pool
 import array
-from wsmtk.whittaker import ws2d, ws2d_vc, ws2d_vc_asy
+from wsmtk.whittaker import lag1corr, ws2d, ws2d_vc, ws2d_vc_asy
 import pickle
 import os
 from cryptography.fernet import Fernet
@@ -469,12 +469,29 @@ def execute_ws2d_sgrid(ix):
 def execute_ws2d_vc(ix):
     '''Execute whittaker smoother with V-curve optimization of s in worker.'''
 
-    if parameters['p']:
+    arr_raw[ix,:], arr_sgrid[ix] = ws2d_vc(y = arr_raw[ix,:], w = np.array((arr_raw[ix,:] != parameters['nd']) * 1,dtype='double'), llas = array.array('d',parameters['srange']))
 
-        arr_raw[ix,:], arr_sgrid[ix] = ws2d_vc_asy(y = arr_raw[ix,:], w = np.array((arr_raw[ix,:] != parameters['nd']) * 1,dtype='double'), llas = array.array('d',parameters['srange']), p = parameters['p'])
-    else:
+    if parameters['shared_array_smooth']:
 
-        arr_raw[ix,:], arr_sgrid[ix] = ws2d_vc(y = arr_raw[ix,:], w = np.array((arr_raw[ix,:] != parameters['nd']) * 1,dtype='double'), llas = array.array('d',parameters['srange']))
+        z2 = parameters['vec_dly'].copy()
+        z2[ z2 != parameters['nd'] ] = arr_raw[ix,:]
+        z2[...] = ws2d(y = z2, lmda = 0.0001, w = np.array((z2 != parameters['nd']) * 1,dtype='double'))
+        arr_smooth[ix,:] = z2[parameters['dix']]
+
+def execute_ws2d_vcp(ix):
+    '''Execute whittaker smoother with asymmetric V-curve optimization of s in worker.'''
+
+    if not parameters['srange']:
+
+        lc = lag1corr(arr_raw[ix,:-1],arr_raw[ix,1:],parameters['nd'])
+
+        if lc <= 0.5:
+            parameters['srange'] = np.linspace(-2,1,16)
+
+        else:
+            parameters['srange'] = np.linspace(0,3,16)
+
+    arr_raw[ix,:], arr_sgrid[ix] = ws2d_vc_asy(y = arr_raw[ix,:], w = np.array((arr_raw[ix,:] != parameters['nd']) * 1,dtype='double'), llas = array.array('d',parameters['srange']), p = parameters['p'])
 
     if parameters['shared_array_smooth']:
 
