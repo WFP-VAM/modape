9380a04f2e88834870a55f3daa2fe557df1abeb8
Valentin Pesendorfer <valentin.pesendorfer@wfp.org>
2020-07-27 11:50:48 +0200

    new download_modis.py

--- modape/scripts/modis_download.py
+++ modape/scripts/modis_download.py
@@ -1,125 +1,181 @@
 #!/usr/bin/env python
 """modis_download.py: Query and download MODIS HDF files."""
 
-import argparse
-from datetime import datetime
+import datetime
 import os
-from pathlib import Path
+import pathlib
+import re
 import sys
-import warnings
+from typing import List
 
+import click
 from modape.modis import ModisQuery
 
-warnings.filterwarnings("default", category=DeprecationWarning)
-
-def main():
-    '''Query and download MODIS products.
+def main(products: List[str],
+         begin_date: datetime.datetime,
+         end_date: datetime.datetime,
+         targetdir: pathlib.Path,
+         roi: str,
+         tile_filter: str,
+         username: str,
+         password: str,
+         strict_dates: bool,
+         return_results: bool,
+         download: bool,
+         multithread: bool,
+         collection: str,
+         ) -> None:
+    """Query and download MODIS products.
 
     This function allows for querying and downloading MODIS products in bulk.
     Multiple products can be queried and downloaded with one
     function call. For downloading data, valid earthdata credentials are required
     (to register, visit https://urs.earthdata.nasa.gov/users/new).
-    Data download can be performed with python's request module
-    or with external ARIA2 (needs to be available in PATH) if --aria2 flag is added.
 
     To query for both MODIS AQUA and TERRA, replace MOD/MYD with M?D.
     Product IDs also accepted in lowercase.
-    '''
 
-    parser = argparse.ArgumentParser(description='Query and download MODIS products (Earthdata account required for download)')
-    parser.add_argument('product', help='MODIS product ID(s)', nargs='+')
-    parser.add_argument('--roi', help='Region of interest. Can be LAT/LON point, bounding box in format (xmin, ymin, xmax, ymax)', nargs='+', required=False)
-    parser.add_argument('--tile-filter', help='MODIS tile filter (download only specified tiles)', nargs='+', required=False, metavar='')
-    parser.add_argument('-c', '--collection', help='MODIS collection', default='006', metavar='')
-    parser.add_argument('-b', '--begin-date', help='Start date (YYYY-MM-DD)', metavar='')
-    parser.add_argument('-e', '--end-date', help='End date (YYYY-MM-DD)', metavar='')
-    parser.add_argument('--username', help='Earthdata username (required for download)', metavar='')
-    parser.add_argument('--password', help='Earthdata password (required for download)', metavar='')
-    parser.add_argument('-d', '--targetdir', help='Destination directory', default=os.getcwd(), metavar='')
-    parser.add_argument('--download', help='Download data', action='store_true')
-    parser.add_argument('--strict-dates', help='Enforce dates strictly', action='store_true')
-    parser.add_argument('--multithread', help='Use multiple threads for downloading', action='store_true')
+    Args:
+        products (List[str]): List of MODIS product codes to download.
+        begin_date (datetime.date): Start date for query / download.
+        end_date (datetime.date): End date for query / download.
+        targetdir (pathlib.Path): Target directory.
+        roi (str): Region of interest (point or bbox as csv string).
+        tile_filter (str): MODIS tile filter (as csv string of tile IDs).
+        username (str): Earthdata username.
+        password (str): Earthdata password.
+        strict_dates (bool): Strict date handling.
+        download (bool): Download data.
+        multithread (bool): Use multiple threads for downloading.
+        collection (str): MODIS collection version.
+    """
 
-    # fail and print help if no arguments supplied
-    if len(sys.argv) == 1:
-        parser.print_help(sys.stderr)
-        sys.exit(0)
+    click.echo("\nSTART download_modis.py!")
 
-    args = parser.parse_args()
+    if download:
 
-    if args.download:
-
-        if not args.username or not args.password:
+        if username is None or password is None:
             raise ValueError("Download was requested, but credentials are missing.")
 
+    if targetdir is None:
+        targetdir = pathlib.Path(os.getcwd())
+
     # handle targetdir
-    target_directory = Path(args.targetdir)
-    target_directory.mkdir(exist_ok=True)
+    targetdir.mkdir(exist_ok=True)
 
-    assert target_directory.exists()
-    assert target_directory.is_dir()
+    assert targetdir.exists()
+    assert targetdir.is_dir()
 
-    products = []
+    products_parsed = []
 
-    for product_code in args.product:
-
-        if "M?D" in product_code:
-            products.append(product_code.replace("?", "O").upper())
-            products.append(product_code.replace("?", "Y").upper())
+    for product_code in products:
+        if "M?D" in product_code.upper():
+            products_parsed.append(product_code.replace("?", "O").upper())
+            products_parsed.append(product_code.replace("?", "Y").upper())
         else:
-            products.append(product_code.upper())
+            products_parsed.append(product_code.upper())
 
-    if args.begin_date:
+    if roi is not None:
+
         try:
-            start = datetime.strptime(args.begin_date, "%Y-%m-%d")
+            coords = list(map(float, roi.split(',')))
         except ValueError:
-            print('Error parsing begin-date!')
-            raise
-    else:
-        start = None
+            click.echo("Error parsing ROI coordinates. Expected numeric!")
 
-    if args.end_date:
-        try:
-            stop = datetime.strptime(args.end_date, "%Y-%m-%d")
-        except ValueError:
-            print('Error parsing begin-date!')
-            raise
-    else:
-        stop = None
+        assert len(coords) == 2 or len(coords) == 4, "Only point or bbox as roi allowed!"
 
-    print('Running query!')
+        roi = tuple(coords)
+
+    # parse tile filter and check if OK
+    if tile_filter is not None:
+        tile_regxp = re.compile(r'^h\d{2}v\d{2}$')
+        tiles = []
+
+        for tile_sel in tile_filter.split(','):
+            assert re.match(tile_regxp, tile_sel.lower())
+            tiles.append(tile_sel.lower())
+
+        tile_filter = tiles
+
+    click.echo('Quering NASA CMR ...')
 
     query = ModisQuery(
-        products=products,
-        aoi=args.roi,
-        begindate=start,
-        enddate=stop,
-        tile_filter=args.tile_filter,
-        version=args.collection,
+        products=products_parsed,
+        aoi=roi,
+        begindate=begin_date,
+        enddate=end_date,
+        tile_filter=tile_filter,
+        version=collection,
     )
 
-    query.search(strict_dates=args.strict_dates)
+    query.search(strict_dates=strict_dates)
 
-    print(f'Found {query.nresults} results!')
+    if query.nresults == 0:
+        click.echo("No results found! Please check query or make sure CMR is available / reachable.")
+        sys.exit(0)
 
-    if args.download:
+    click.echo(f'Done! Found {query.nresults} results!')
 
-        print('Downloading!')
+    if return_results:
+        click.echo("\n")
+        for res in query.results:
+            click.echo(res)
+        click.echo("\n")
+
+    if download:
+
+        click.echo('Downloading!')
 
         if query.nresults > 0:
 
             query.download(
-                targetdir=target_directory,
-                username=args.username,
-                password=args.password,
-                multithread=args.multithread
+                targetdir=targetdir,
+                username=username,
+                password=password,
+                multithread=multithread
             )
 
-        else:
-            print('Nothing to download! Exiting ... ')
 
+    click.echo('modis_download.py COMPLETED! Bye! \n')
 
-    print('Done!')
+@click.command()
+@click.argument("products", nargs=-1, type=click.STRING)
+@click.option("-b", "--begin-date", type=click.DateTime(formats=["%Y-%m-%d"]), help="Start date for query")
+@click.option("-e", "--end-date", type=click.DateTime(formats=["%Y-%m-%d"]), help="End date for query")
+@click.option("-d", "--targetdir", type=click.Path(dir_okay=True, writable=True, resolve_path=True),
+              help="Destination directory for downloaded files")
+@click.option("--roi", type=click.STRING, help="Region of interest. Either LAT,LON or xmin,ymin,xmax,ymax")
+@click.option("--tile-filter", type=click.STRING, help="Filter tiles - supplied as csv list")
+@click.option("--username", type=click.STRING, help="Earthdata username")
+@click.option("--password", type=click.STRING, help="Earthdata password")
+@click.option("--strict-dates", is_flag=True, help="Don't allow files with timestamps outside of provided date(s)")
+@click.option("--return-results", is_flag=True, help="Print results to console")
+@click.option("--download", is_flag=True, help="Download data")
+@click.option("--multithread", is_flag=True, help="Use multiple threads for downloading")
+@click.option("-c", "--collection", type=click.STRING, default="006", help="MODIS collection")
+def cli(products, begin_date, end_date, targetdir, roi, tile_filter, username, password, strict_dates, return_results, download, multithread, collection):
+    """modis_download.py: Query and download MODIS HDF files."""
+
+    if len(sys.argv) == 1:
+        cli.main(['--help'])
+    else:
+        main(products,
+             begin_date,
+             end_date,
+             targetdir,
+             roi,
+             tile_filter,
+             username,
+             password,
+             strict_dates,
+             return_results,
+             download,
+             multithread,
+             collection)
 
 if __name__ == '__main__':
-    main()
+
+    if len(sys.argv) == 1:
+        cli.main(['--help'])
+    else:
+        cli() #pylint: disable=E1120
