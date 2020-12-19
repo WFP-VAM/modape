34aa8f07efb8fe02e7db626f4b83f33695fa02aa
Valentin Pesendorfer <valentin.pesendorfer@wfp.org>
2020-12-19 19:39:59 +0100

    fix typing

--- modape/scripts/modis_window.py
+++ modape/scripts/modis_window.py
@@ -8,7 +8,7 @@ import logging
 from pathlib import Path
 import re
 import sys
-from typing import Tuple
+from typing import Dict, List, Tuple, Union
 
 import click
 from modape.constants import REGEX_PATTERNS
