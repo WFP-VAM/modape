6eeed7d7b322033e74e81b779d5a0fb52dc9bc4e
Rob <rob.marjot@gmail.com>
2021-03-24 23:25:44 +0100

    enhancements to the development enironment

--- chain/.devcontainer/setup_modape.sh
+++ chain/.devcontainer/setup_modape.sh
@@ -3,6 +3,7 @@ if [ -f "../../setup.py" ]
 then
     cd ../..
 else
+    echo "Cloning MODAPE from WFP-VAM repository..."
     cd /workspaces && git clone https://github.com/WFP-VAM/modape.git && cd modape && git checkout -b v1.0rc origin/v1.0rc
 fi
 pip install .
--- chain/wsgi.py
+++ chain/wsgi.py
@@ -1,2 +1,6 @@
+"""
+    Run as Gunicorn service from within the container: gunicorn --workers=1 --threads=1 --bind 0.0.0.0:5001 wsgi:arc_modis_ndvi_server
+"""
+
 from arc_modis_ndvi import app_setup
 arc_modis_ndvi_server = app_setup('production.json')
