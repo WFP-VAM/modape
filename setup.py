ae1f3e1f2dcc37eac08a09c87de1e0e72f016b35
valpesendorfer <v.p@gmx.at>
2022-09-06 11:34:26 +0200

    pin click

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "click"],
     install_requires=[
-        "click>=7.1",
+        "click<8",
         "numpy>=1.16.1",
         "gdal>=2",
         "h5py>=2.9",
